85da3c05078734c9756862d4eb49c04724073ea0
pavankumarreddy15 <55206955+pavankumarreddy15@users.noreply.github.com>
2022-05-16 11:46:32 +0530

    Update wrapper.py

--- wrapper.py
+++ wrapper.py
@@ -475,7 +475,7 @@ if __name__ == "__main__":
     train_dataset = [train_set['images'][i].astype(np.float32) for i in range(len(train_set['images']))]
     test_dataset = [[test_set['images'][i].astype(np.float32),test_set['labels'][i]] for i in range(len(test_set['images']))]
     test_x = torch.Tensor([np.array(test_set['images'][i].astype(np.float32)) for i in range(len(test_set['images']))])
-    test_y = torch.Tensor([np.array(test_set['labels'][i]) for i in range(len(test_set['images']))])
+    test_y = torch.Tensor([np.array([test_set['labels'][i]]) for i in range(len(test_set['images']))])
     my_testdataset = TensorDataset(test_x,test_y)
     test_loader = DataLoader(my_dataset)
     labeled_dataset = [[train_set['images'][i].astype(np.float32),train_set['labels'][i]] for i in range(len(train_set['images']))]
