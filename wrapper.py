3b6da924384cf5497200bfdd384c2774d9d0d60c
pavankumarreddy15 <55206955+pavankumarreddy15@users.noreply.github.com>
2022-05-16 12:30:36 +0530

    Update wrapper.py

--- wrapper.py
+++ wrapper.py
@@ -124,9 +124,9 @@ class CustomDataset(Dataset):
         super().init(self,transform=None,target_transform=None)
     def __getitem__(self,index):
         img, target = self.data[index], self.targets[index]
-	if self.transform is not None:
-	    img = self.transform(img)
-	return img,target
+        if self.transform is not None:
+        img = self.transform(img)
+        return img,target
       
 class TransformFixMatch(object):
     def __init__(self, mean, std):
