cc149a89e2df9af68ea13283e72e9d90f2de45c0
pavankumarreddy15 <55206955+pavankumarreddy15@users.noreply.github.com>
2022-05-17 00:12:03 +0530

    Update wrapper.py

--- wrapper.py
+++ wrapper.py
@@ -140,11 +140,9 @@ class MyDataset(Dataset):
             self.data = data
             self.transform = transform
     def __getitem__(self, index):
-        if self.labeled:
-            x = self.data[index]
-            y = self.targets[index]
-        else:
-            x = self.data[index]
+        x = self.data[index]
+        y = self.targets[index]
+        
         
         if self.transform:
             x = Image.fromarray(np.array(self.data[index]).astype(np.uint8).transpose(1,2,0))
